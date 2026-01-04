110f79c34264d58a27f0dd5a44f5b592e43d9bd2
jacksonandrers <upmarketingassessoria@gmail.com>
2026-01-04 10:33:44 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        primary: '#10B981',
-      }
+theme: {
+  extend: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
     },
   },
-  plugins: [],
-}
+},
